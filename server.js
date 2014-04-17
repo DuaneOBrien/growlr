fb6d1c04da080d3cfaf913489a19ffb39af354ef
Duane O'Brien <duane.obrien@gmail.com>
2014-04-17 09:22:37 -0400

    Formats the articles into an RSS feed.  Includes a super-simple server to spit it out.

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ var RSS         = require('rss'),
     FeedParser  = require('feedparser'),
     request     = require('request'),
     fs          = require('fs'),
+    http        = require('http'),
     ArticleCollection = require('./ArticleCollection'),
     feedCount = 0,
     feedParser,
-    req;
+    req,
+    rollup,
+    outputXML,
+    server;
 
 function handleError(error) {
     throw error;
@@ -29,8 +33,29 @@ function handleStreamReadable() {
 function handleFeedEnd() {
     feedCount -= 1;
     if (feedCount <= 0) {
-        console.log(ArticleCollection.getArticles());
+        generateRollup(ArticleCollection.getArticles());
     }
+}
+
+function generateRollup(articles) {
+    var article;
+    rollup = new RSS({
+        title: 'Growlr Rollup',
+        feed_url: 'http://somefakededwebsite.notadomain/growlr.rss',
+        site_url: 'http://somefakededwebsite.notadomain/',
+        author: 'Some Fake Author'
+    });
+    for (article in articles) {
+        if (articles.hasOwnProperty(article)) {
+            rollup.item({
+                title: articles[article].title,
+                description: articles[article].description,
+                url: articles[article].url,
+                date: articles[article].date,
+            });
+        }
+    }
+    outputXML = rollup.xml();
 }
 
 fs.readFile('feeds.json', {encoding: 'UTF8'}, function (error, data) {
@@ -50,5 +75,15 @@ fs.readFile('feeds.json', {encoding: 'UTF8'}, function (error, data) {
         }
     }
 });
+
+server = http.createServer(function (request, response) {
+    if (outputXML) {
+        response.writeHead(200, {"Content-Type": "application/rss+xml"});
+        response.end(outputXML);
+    }
+});
+
+server.listen(8064);
+
 
 
