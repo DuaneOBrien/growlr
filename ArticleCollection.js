6e7004142ff2e1c5ce9968d91791693a68370b17
Duane O'Brien <duane.obrien@gmail.com>
2014-04-16 23:15:55 -0400

    Sort returned articles by pubDate

--- ArticleCollection.js
+++ ArticleCollection.js
@@ -7,8 +7,20 @@ function init() {
     articles = [];
 }
 
+function sortByDateDesc(a, b) {
+    var aComp = Date.parse(a.pubDate),
+        bComp = Date.parse(b.pubDate);
+    if (aComp < bComp) {
+        return 1;
+    }
+    if (aComp > bComp) {
+        return -1;
+    }
+    return 0;
+}
+
 function getArticles() {
-    return articles;
+    return articles.sort(sortByDateDesc);
 }
 
 function addArticle(title, summary, link, pubDate) {
--- tests/specs/main-Spec.js
+++ tests/specs/main-Spec.js
@@ -19,10 +19,26 @@
             expect(ArticleCollection.addArticle('test title', 'test summary', 'alink', 'today')).toBe(1);
             expect(ArticleCollection.getArticles()).toEqual([{title: 'test title', summary: 'test summary', link: 'alink', pubDate: 'today'}]);
         });
+        it('returns the article count when it pushes in a new onw', function () {
+            expect(ArticleCollection.getArticles()).toEqual([]);
+            expect(ArticleCollection.addArticle('Article 1', 'test summary', 'alink', 'today')).toBe(1);
+            expect(ArticleCollection.addArticle('Article 2', 'test summary', 'alink', 'today')).toBe(2);
+        });
         it('reinitializes to an empty list', function () {
             expect(ArticleCollection.addArticle('test title', 'test summary', 'alink', 'today')).toBe(1);
             ArticleCollection.init();
             expect(ArticleCollection.getArticles()).toEqual([]);
+        });
+        it('sorts articles by descending pub date before returning', function () {
+            var expectedResults = [
+                {title: 'Third Article', summary: 'Third Summary', link: 'Third Link', pubDate: 'Tue Apr 08 2012 18:56:37 GMT-0500 (EST)'},
+                {title: 'Second Article', summary: 'Second Summary', link: 'Second Link', pubDate: 'Tue Mar 07 2012 18:56:37 GMT-0500 (EST)'},
+                {title: 'First Article', summary: 'First Summary', link: 'First Link', pubDate: 'Tue Mar 06 2012 18:56:37 GMT-0500 (EST)'}
+            ];
+            expect(ArticleCollection.addArticle('Second Article', 'Second Summary', 'Second Link', 'Tue Mar 07 2012 18:56:37 GMT-0500 (EST)')).toBe(1);
+            expect(ArticleCollection.addArticle('Third Article', 'Third Summary', 'Third Link', 'Tue Apr 08 2012 18:56:37 GMT-0500 (EST)')).toBe(2);
+            expect(ArticleCollection.addArticle('First Article', 'First Summary', 'First Link', 'Tue Mar 06 2012 18:56:37 GMT-0500 (EST)')).toBe(3);
+            expect(ArticleCollection.getArticles()).toEqual(expectedResults);
         });
     });
 }());
